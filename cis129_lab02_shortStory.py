fe1600b6f7725a9fc8e99898796cbc18961b95af
AveryDurfee <159318160+AveryDurfee@users.noreply.github.com>
2024-02-09 21:22:24 -0700

    Update cis129_lab02_shortStory.py
    Added poem of dubious quality

--- cis129_lab02_shortStory.py
+++ cis129_lab02_shortStory.py
@@ -1,1 +1,8 @@
-print('Hello, World!')
+#Avery Durfee
+#An interesting poem for a lab for class
+#Edits added the poem and deleted the hello world line
+
+print("There once was a bottle of fleas")
+print("That got beat up by mean gangster bees")
+print("The bottle said geez, and pulled off a squeeze")
+print("And now the bees hurt in their knees")
